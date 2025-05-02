d55cfae88e0fc7f2e48065fddd4164b1a5d66193
JaatDevloper <pinterestjat2022@gmail.com>
2025-05-02 22:15:04 +0530

    Update simple_bot.py

--- simple_bot.py
+++ simple_bot.py
@@ -206,7 +206,7 @@ logging.basicConfig(
 logger = logging.getLogger(__name__)
 
 # Bot token from environment variable
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "7631768276:AAHVSKC6xkitG6JhutIRxumfWTowIEP7J7w")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "7631768276:AAHQtIaRje8ZIGMCosEAwaIDRrgNmDi4U8k")
 
 # Conversation states
 QUESTION, OPTIONS, ANSWER, CATEGORY = range(4)
